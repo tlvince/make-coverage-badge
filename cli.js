51138e825c6109892d6777f1d2a0db7310ac6371
pamepeixinho <pameiupip@gmail.com>
2018-12-24 18:53:30 -0200

    add eslint

--- cli.js
+++ cli.js
@@ -3,10 +3,36 @@ const mkdirp = require('mkdirp');
 const { get } = require('https');
 const { readFile, writeFile } = require('fs');
 
-const outputPath = _findArgument('output', './coverage');
-const inputPath = _findArgument('input', './coverage/coverage-summary.json');
+/**
+ * Will lookup the argument in the cli arguments list and will return a
+ * value passed as CLI arg (if found)
+ * Otherwise will return default value passed
+ * @param argName - name of hte argument to look for
+ * @param defaultOutput - default value to return if could not find argument in cli command
+ * @private
+ */
+const findArgument = (argName, defaultOutput) => {
+  if (!argName) {
+    return defaultOutput;
+  }
 
-const getColour = coverage => {
+  const index = process.argv.findIndex(a => a.match(argName))
+  if (index < 0) {
+    return defaultOutput;
+  }
+
+  try {
+    return process.argv[index + 1];
+  } catch (e) {
+    return defaultOutput;
+  }
+}
+
+
+const outputPath = findArgument('output', './coverage');
+const inputPath = findArgument('input', './coverage/coverage-summary.json');
+
+const getColour = (coverage) => {
   if (coverage < 80) {
     return 'red';
   }
@@ -32,25 +58,35 @@ const getBadge = (report, key) => {
 }
 
 const download = (url, cb) => {
-  get(url, res => {
+  get(url, (res) => {
     let file = '';
-    res.on('data', chunk => (file += chunk));
+    res.on('data', (chunk) => {
+      file += chunk;
+    });
     res.on('end', () => cb(null, file));
   }).on('error', err => cb(err));
 }
 
-const getBadgeByKey = (report) => (key) => {
+const writeBadgeInFolder = (key, res) => {
+  writeFile(`${outputPath}/badge-${key}.svg`, res, 'utf8', (writeError) => {
+    if (writeError) {
+      throw writeError;
+    }
+  });
+}
+
+const getBadgeByKey = report => (key) => {
   const url = getBadge(report, key);
 
   download(url, (err, res) => {
-    if (err) throw err
-    mkdirp(outputPath, function (err) {
-      if (err) {
-        console.error(`Could not create output directory ${err}`);
+    if (err) {
+      throw err;
+    }
+    mkdirp(outputPath, (folderError) => {
+      if (folderError) {
+        console.error(`Could not create output directory ${folderError}`);
       } else {
-        writeFile(`${outputPath}/badge-${key}.svg`, res, 'utf8', err => {
-          if (err) throw err;
-        })
+        writeBadgeInFolder(key, res);
       }
     })
   })
@@ -58,32 +94,9 @@ const getBadgeByKey = (report) => (key) => {
 
 readFile(`${inputPath}`, 'utf8', (err, res) => {
   if (err) {
-    throw err
+    throw err;
   }
+
   const report = JSON.parse(res);
   reportKeys.forEach(getBadgeByKey(report));
-})
-
-/**
- * Will lookup the argument in the cli arguments list and will return a value passed as CLI arg (if found)
- * Otherwise will return default value passed
- * @param argName - name of hte argument to look for
- * @param defaultOutput - default value to return if could not find argument in cli command
- * @private
- */
-function _findArgument (argName, defaultOutput) {
-  if (!argName) {
-    return defaultOutput;
-  }
-
-  const index = process.argv.findIndex(a => a.match(argName))
-  if (index < 0) {
-    return defaultOutput;
-  }
-
-  try {
-    return process.argv[index + 1];
-  } catch (e) {
-    return defaultOutput;
-  }
-}
+});
